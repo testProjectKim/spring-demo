e81cb52044677dc165bf94d8c04ecfea61cf8666
kky1234 <gomsir@inforex.co.kr>
2018-11-21 16:02:58 +0900

    db 관련

---
+++ src/test/java/com/example/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.webservice.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void getPostTest(){
+        postsRepository.save(Posts.builder().title("테스트 게시글").content("테스트 본문").author("gomsir@hanmail.net").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+}
