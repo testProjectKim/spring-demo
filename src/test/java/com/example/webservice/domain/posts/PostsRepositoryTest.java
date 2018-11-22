22ba18ecf9e527edde3be50fc07ca41febddce46
kky1234 <gomsir@inforex.co.kr>
2018-11-22 11:09:23 +0900

    화면단

--- src/test/java/com/example/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/webservice/domain/posts/PostsRepositoryTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,4 +36,20 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
     }
+
+
+    @Test
+    public void baseTimeTest(){
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder().title("테스트 게시글").content("테스트 본문").author("gomsir@hanmail.net").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+
 }
